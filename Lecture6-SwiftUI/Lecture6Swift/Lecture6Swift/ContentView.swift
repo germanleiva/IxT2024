ec1cedbe304c76625a26698b0441d8a9b29db3f1
VittoriaFrau <vittoria.frau@unica.it>
2024-10-25 16:10:53 +0200

    Class 6 - Swift UI weather app

---
+++ Lecture6-SwiftUI/Lecture6Swift/Lecture6Swift/ContentView.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  Lecture6Swift
+//
+//  Created by Vittoria Frau on 25/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.blue)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Aarhus, DK")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .fontWeight(.medium)
+                
+                Image(systemName: "cloud.sun.fill")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180, height: 180)
+                
+                Text("10°C")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .fontWeight(.medium)
+                
+                Spacer()
+                
+                HStack (spacing: 20){
+                    WeatherDayView(dayOfWeek: "TUE", icon: "cloud.sun.fill", temperature: "10°C")
+                    WeatherDayView(dayOfWeek: "WED", icon: "cloud.heavyrain.fill", temperature: "8°C")
+                    WeatherDayView(dayOfWeek: "THU", icon: "cloud.bolt.fill", temperature: "5°C")
+                    WeatherDayView(dayOfWeek: "FRI", icon: "sun.max.fill", temperature: "12°C")
+                    WeatherDayView(dayOfWeek: "SAT", icon: "tornado", temperature: "10°C")
+                    WeatherDayView(dayOfWeek: "SUN", icon: "snow", temperature: "8°C")
+                    
+                }
+                Spacer()
+                
+                Button("Tap me",
+                       action: {
+                    print("I pressed the button")
+                }
+                )
+                .frame(width: 180, height: 50)
+                .font(.system(size: 20, weight: .medium))
+                .background(Color.white)
+                .cornerRadius(20)
+                Spacer()
+            }
+            
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct WeatherDayView: View {
+    var dayOfWeek : String
+    var icon : String
+    var temperature : String
+    
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .foregroundColor(.white)
+                .fontWeight(.medium)
+                .font(.title3)
+            Image(systemName: icon)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text(temperature)
+                .foregroundColor(.white)
+                .fontWeight(.medium)
+                .font(.title3)
+        }
+    }
+}
+
